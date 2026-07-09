6387ed2df6f18e0731809f542e6e8e5b8138bb72
ubaydulloh <lazizbekxoljigitov@gmail.com>
2026-07-09 10:43:00 +0500

    sanr

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 import { ref } from "vue";
+import HelloWorld from "./components/HelloWorld.vue";
 
 const count = ref(0);
 </script>
 
 <template>
+  <HelloWorld />
   <h1>{{ count }}</h1>
   <button @click="count++">+</button>
 </template>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,5 @@
+<template>
+  <h1>Frontedn</h1>
+  <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam, officiis fuga?</p>
+  <button>ABout</button>
+</template>
