f789285b67947d6540eeb54e0458a619ed52a6b0
ubaydulloh dadakxanov <lazizbekxoljigitov@gmail.com>
2026-07-11 20:26:29 +0500

    sanr

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -5,7 +5,7 @@ import HeroBottom from '../templates/HeroBottom.vue';
 
 <template>
   <div class="flex flex-col w-full gap-[30px]">
-    <div class="bg-[#1B3764] w-full h-full sm:h-screen pt-[130px] pb-[20px]">
+    <div class="bg-[#1B3764] w-full h-full sm:h-full pt-[130px] pb-[20px]">
       <div class="contenr">
         <Hero />
       </div>
