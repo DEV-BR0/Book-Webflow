b7900e567deb4495b4a411bb4d6d318503589e2a
ubaydulloh dadakxanov <lazizbekxoljigitov@gmail.com>
2026-07-11 21:01:47 +0500

    sanr

--- src/templates/HeroBottom.vue
+++ src/templates/HeroBottom.vue
@@ -1,74 +1,61 @@
 <template>
-  <div data-aos="fade-up" class="flex justify-center flex-col w-full h-[70vh] items-center gap-[70px] ">
-    <h1 class="text-[#1B3764] font-[innes] text-[46px] font-bold">
+  <div
+    data-aos="fade-up"
+    class="flex justify-center flex-col w-full h-[70vh] items-center gap-[70px]"
+  >
+    <h1
+      class="text-[#1B3764] font-[innes] text-[23px] md:text-[46px] font-bold"
+    >
       Author’s Book Includes
     </h1>
-    <div class="flex w-full justify-between gap-[70px] ">
+    <div class="flex w-full justify-between gap-[70px]">
+
+
+      {{ 
+        
+        
+        
+        
+        
+        }}
       <div class="flex grow lg:flex-row flex-col">
-        <img src="../images/one.png" alt="Book-Photo" class="w-[200px] lg:w-auto " />
+        <img
+          src="../images/one.png"
+          alt="Book-Photo"
+          class="w-[200px] lg:w-auto"
+        />
         <div class="flex flex-col gap-[20px]">
-          <p class="text-[#1B3764] text-[32px]">Atomic One’s</p>
-          <p class="line-clamp-3 text-[#969AA0] text-[19px] max-w-[300px]">
-            Many variations of passages of Lorem Ipsum willing araise alteration
-            in some form.
+          <p class="text-[#1B3764] md:text-[32px] text-[19px] font-bold">
+            Atomic One’s
           </p>
-          <div class="flex">
-            <div class="flex gap-[20px] grow text-[#1B3764]">
-              <div
-                class="w-[16px] h-[16px] bg-[#FFCA42] rounded-[50%] mt-[8px]"
-              ></div>
-              <div class="flex flex-col gap-[10px]">
-                <p class="font-[innes] text-[21px]">Pages:</p>
-                <p>586pages</p>
-              </div>
-            </div>
-            <div class="flex gap-[20px] grow text-[#1B3764]">
-              <div
-                class="w-[16px] h-[16px] bg-[#FFCA42] rounded-[50%] mt-[8px]"
-              ></div>
-              <div class="flex flex-col gap-[10px]">
-                <p class="font-[innes] text-[21px]">Length:</p>
-                <p>10 Hours</p>
-              </div>
-            </div>
-          </div>
-          <button
-            class="border-[#FFCA42] border border-1 text-[#1B3764] hover:text-white  hover:bg-[#1B3764] pt-[14px] pb-[14px] pl-[20px] pr-[20px] font-[inner] hover:rounded-2xl transition-all duration-200"
+          <p
+            class="line-clamp-3 text-[#969AA0] md:text-[19px] text-[15px] max-w-[300px]"
           >
-            Order Today
-          </button>
-        </div>
-      </div>
-      <div class="flex grow lg:flex-row flex-col">
-        <img src="../images/two.png" alt="Book-Photo" class="w-[200px]  lg:w-auto"/>
-        <div class="flex flex-col gap-[20px]">
-          <p class="text-[#1B3764] text-[32px]">Atomic One’s</p>
-          <p class="line-clamp-3 text-[#969AA0] text-[19px] max-w-[300px]">
             Many variations of passages of Lorem Ipsum willing araise alteration
             in some form.
           </p>
           <div class="flex">
-            <div class="flex gap-[20px] grow text-[#1B3764]">
+            <div class="flex md:gap-[20px] gap-[7px] grow text-[#1B3764]">
               <div
-                class="w-[16px] h-[16px] bg-[#FFCA42] rounded-[50%] mt-[8px]"
+                class="md:w-[16px] md:h-[16px] w-[7px] h-[7px] bg-[#FFCA42] rounded-[50%] mt-[8px]"
               ></div>
               <div class="flex flex-col gap-[10px]">
-                <p class="font-[innes] text-[21px]">Pages:</p>
+                <p class="font-[innes] md:text-[21px] text-[14px]">Pages:</p>
                 <p>586pages</p>
               </div>
             </div>
-            <div class="flex gap-[20px] grow text-[#1B3764]">
+            <div class="flex md:gap-[20px] gap-[7px] grow text-[#1B3764]">
               <div
-                class="w-[16px] h-[16px] bg-[#FFCA42] rounded-[50%] mt-[8px]"
+                class="md:w-[16px] md:h-[16px] w-[7px] h-[7px] bg-[#FFCA42] rounded-[50%] mt-[8px]"
               ></div>
               <div class="flex flex-col gap-[10px]">
-                <p class="font-[innes] text-[21px]">Length:</p>
+                <p class="font-[innes] md:text-[21px] text-[14px]">Length:</p>
                 <p>10 Hours</p>
               </div>
             </div>
           </div>
           <button
-            class="border-[#FFCA42] border border-1 text-[#1B3764] hover:text-white  hover:bg-[#1B3764] pt-[14px] pb-[14px] pl-[20px] pr-[20px] font-[inner] hover:rounded-2xl transition-all duration-200"
+            class="border-[#FFCA42] border border-1 text-[#1B3764] hover:text-white hover:bg-[#1B3764] md:pt-[14px] md:pb-[14px] md:pl-[20px] md:pr-[20px] p-[10px] font-[inner] hover:rounded-2xl transition-all duration-200"
           >
             Order Today
           </button>
