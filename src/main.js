eb2c5f14ded6681823f5d49ec75ab0ab0d14df75
ubaydulloh dadakxanov <lazizbekxoljigitov@gmail.com>
2026-07-14 22:56:36 +0500

    sanr

--- src/main.js
+++ src/main.js
@@ -7,7 +7,6 @@ import "aos/dist/aos.css";
 
 AOS.init({
   duration: 1500,
-  once: true,
 });
 
 createApp(App).use(router).mount("#app");
--- src/templates/Home/Author.vue
+++ src/templates/Home/Author.vue
@@ -1,10 +1,24 @@
 <template>
-  <div data-aos="fade-up" class="flex justify-between w-full h-full">
+  <div data-aos="fade-up" class="flex md:justify-between w-full h-full flex-col md:flex-row justify-center">
     <!-- left -->
     <div class="relative flex flex-col grow">
-      <img src="../../../public/images/left_one.png" alt="photo" class="md:h-[60vh] h-[30vh]" />
-      <div class="absolute -z-40 border-1 border-amber-400 w-full h-full"></div>
+      <div class="flex grow">
+        <img
+          src="../../../public/images/left_one.png"
+          alt="photo"
+          class="md:h-[60vh] w-[60%] flex grow-[0.5] h-[30%]"
+        />
+        <div
+          class="absolute -z-40 border-[12px] w-[85%] flex grow-[0.7] border-amber-400  h-full bottom-[-30px]"
+        ></div>
+      </div>
     </div>
-    <div class="flex flex-col">vbnfdsjk</div>
+    <div class="flex flex-col grow gap-[40px]">
+      <div class="title ">
+        <p class="text-[45px] font-[innes]  text-[#1B3764]">About Author</p>
+        <div class="border-1 border-[#FFCA42] h-[1px]"></div>
+      </div>
+      <p class="line-cap max-w-[567px] line-clamp-3">All the Lorem Ipsum generators on the Internet tend to repeated predefined chunks as necessary, making this the first true value generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful.</p>
+    </div>
   </div>
 </template>
