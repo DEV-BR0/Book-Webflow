25bdacd513473265a19712bcad55383b2902b394
ubaydulloh dadakxanov <lazizbekxoljigitov@gmail.com>
2026-07-14 23:03:01 +0500

    sanr

--- src/templates/Home/Author.vue
+++ src/templates/Home/Author.vue
@@ -1,5 +1,8 @@
 <template>
-  <div data-aos="fade-up" class="flex md:justify-between w-full h-full flex-col md:flex-row justify-center">
+  <div
+    data-aos="fade-up"
+    class="flex md:justify-between w-full h-full flex-col md:flex-row justify-center gap-[60px]"
+  >
     <!-- left -->
     <div class="relative flex flex-col grow">
       <div class="flex grow">
@@ -9,16 +12,27 @@
           class="md:h-[60vh] w-[60%] flex grow-[0.5] h-[30%]"
         />
         <div
-          class="absolute -z-40 border-[12px] w-[85%] flex grow-[0.7] border-amber-400  h-full bottom-[-30px]"
+          class="absolute -z-40 border-[12px] w-[85%] flex grow-[0.7] border-amber-400 h-full bottom-[-30px]"
         ></div>
       </div>
     </div>
     <div class="flex flex-col grow gap-[40px]">
-      <div class="title ">
-        <p class="text-[45px] font-[innes]  text-[#1B3764]">About Author</p>
+      <div class="title">
+        <p class="text-[45px] font-[innes] text-[#1B3764]">About Author</p>
         <div class="border-1 border-[#FFCA42] h-[1px]"></div>
       </div>
-      <p class="line-cap max-w-[567px] line-clamp-3">All the Lorem Ipsum generators on the Internet tend to repeated predefined chunks as necessary, making this the first true value generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful.</p>
+      <p class="line-cap max-w-[567px] line-clamp-3">
+        All the Lorem Ipsum generators on the Internet tend to repeated
+        predefined chunks as necessary, making this the first true value
+        generator on the Internet. It uses a dictionary of over 200 Latin words,
+        combined with a handful.
+      </p>
+      <div class="flex">
+        <div class="flex flex-col ">
+          <p class="md:text-[55px] text-[26px] font-[innes] font-bold text-[#1B3764]">02</p>
+          <p class=" md:text-[18px] text-[14px] text-[#969AA0]">Books Published</p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
