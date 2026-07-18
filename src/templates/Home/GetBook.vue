20d95301441136839c0a2427a6c88ab8f72444f4
ubaydulloh <lazizbekxoljigitov@gmail.com>
2026-07-18 20:59:35 +0500

    sanr

--- src/templates/Home/GetBook.vue
+++ src/templates/Home/GetBook.vue
@@ -6,6 +6,12 @@
       <div class="flex grow flex-col justify-center">
         <h2 class="font-bold text-[45px] text-white">Get Book Copy Today!</h2>
         <div class="h-[2px] bg-[#FFCA42] w-[100px]"></div>
+        <p
+          class="text-[#B4C7E7] md:text-[19px] text-[15px] line-clamp-2 max-w-[500px]"
+        >
+          This the first true value generator on the Internet. It uses alphas
+          dictionary of over 200 Latin words.
+        </p>
       </div>
     </div>
   </div>
