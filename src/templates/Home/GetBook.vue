2778b2527033b6a100dfd6bd97e9176f69606ee4
ubaydulloh <lazizbekxoljigitov@gmail.com>
2026-07-18 20:47:56 +0500

    sanr

--- src/templates/Home/GetBook.vue
+++ src/templates/Home/GetBook.vue
@@ -1,5 +1,10 @@
 <template>
   <div data-aos="fade-up" class="bg-[#1B3764] w-full h-[500px]">
-    <div class="contenr"></div>
+    <div class="contenr">
+
+      <div class="flex grow flex-col">
+        
+      </div>
+    </div>
   </div>
 </template>
