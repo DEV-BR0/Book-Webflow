e612c647f5c1e5890497b079a36af59efaffeac4
ubaydulloh <lazizbekxoljigitov@gmail.com>
2026-07-18 20:55:31 +0500

    sanr

--- src/templates/Home/GetBook.vue
+++ src/templates/Home/GetBook.vue
@@ -1,9 +1,11 @@
 <template>
   <div data-aos="fade-up" class="bg-[#1B3764] w-full h-[500px]">
-    <div class="contenr">
-
-      <div class="flex grow flex-col">
-        <h2>Get Book Copy Today!</h2>
+    <div
+      class="contenr h-full flex md:flex-row flex-col items-center justify-center"
+    >
+      <div class="flex grow flex-col justify-center">
+        <h2 class="font-bold text-[45px] text-white">Get Book Copy Today!</h2>
+        <div class="h-[2px] bg-[#FFCA42] w-[100px]"></div>
       </div>
     </div>
   </div>
